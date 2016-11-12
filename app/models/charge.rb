6cf669664a609d9a878757cbeefe27147e7f068d
Raghu Betina <raghu@firstdraft.com>
2016-11-12 02:29:09 +0000

    Direct association between users and charges

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,6 +1,9 @@
 class Charge < ApplicationRecord
   # Direct associations
 
+  belongs_to :payer,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :charges_paid,
+             :class_name => "Charge",
+             :foreign_key => "payer_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
